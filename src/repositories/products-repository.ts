4cf0b5b0eba7db43417fc25637678f531a6d705c
flavioaugusto1 <f.augustosdn@gmail.com>
2025-05-28 22:45:05 -0300

    feat: deletar produto

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -2,8 +2,9 @@ import { Prisma, Product } from '@prisma/client'
 
 export interface ProductsRepository {
   create(data: Prisma.ProductCreateInput): Promise<Product>
+  update(data: Prisma.ProductCreateInput): Promise<void>
+  delete(id: string): Promise<void>
   searchProducts(query: string, page: number): Promise<Product[]>
   fetchProducts(): Promise<Product[]>
-  update(data: Prisma.ProductCreateInput): Promise<void>
   getProductById(id: string): Promise<Product | null>
 }
