0a39f759fff6660a0d02b0e17f9dae3a9bf6635c
flavioaugusto1 <f.augustosdn@gmail.com>
2025-05-31 17:12:02 -0300

    feat: jwt verify middleware

--- src/controllers/http/products/products-controller.ts
+++ src/controllers/http/products/products-controller.ts
@@ -3,8 +3,10 @@ import { createProductsController } from './create-products-controller'
 import { getProductByIdController } from './get-product-by-id-controller'
 import { fetchProdcuts } from './fetch-products-controller'
 import { deleteProductsController } from './delete-products-controller'
+import { verifyJWT } from '../../middlewares/verify-jwt'
 
 export function productsController(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
   app.post('/new', createProductsController)
   app.get('/:id', getProductByIdController)
   app.get('/list', fetchProdcuts)
