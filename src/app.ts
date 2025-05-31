6c0bd2b4c3d128824010b0ed83cba45d400ed21c
flavioaugusto1 <f.augustosdn@gmail.com>
2025-05-31 17:13:04 -0300

    feat: error handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { env } from './env'
 import { usersController } from './controllers/http/users/users-controller'
+import { productsController } from './controllers/http/products/products-controller'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -10,3 +12,16 @@ app.register(fastifyJwt, {
 })
 
 app.register(usersController, { prefix: '/user' })
+app.register(productsController, { prefix: '/products' })
+
+app.setErrorHandler((error, request, response) => {
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .send({ message: 'Validation error!', issue: error.format() })
+  }
+
+  return response.status(500).send({
+    message: 'Internal server error!',
+  })
+})
